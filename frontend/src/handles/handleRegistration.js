b2b250ae5613185ab74631e455837468b93f9471
João Sequeira <joaosequeira@student.uc.pt>
2025-06-09 14:23:48 +0100

    Frontend - resolved toast notifications problems. Have to implement better error handling in api calls and notifications

--- frontend/src/handles/handleRegistration.js
+++ frontend/src/handles/handleRegistration.js
@@ -5,10 +5,8 @@ const handleRegistration = async (newUser, intl) => {
   const response = await register(newUser);
 
   if (response.success) {
-    console.log("Registration successful:", response.data);
     return true; // Success
   } else {
-    console.warn("Registration failed:", response.status);
     handleApiError(response.error, intl); // Handle error here
     return false; // Failure
   }
